8c5f62a35a067a32bf9248c424ec4528de6e0da9
Victor Yang <victoryang@Victors-M1Pro-3.local>
2024-01-31 18:01:41 -0600

    init

---
+++ src/main/java/hongtay/example/udemyspringboot/UdemySpringbootApplication.java
@@ -0,0 +1,13 @@
+package hongtay.example.udemyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hongtay/example/udemyspringboot/UdemySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package hongtay.example.udemyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
